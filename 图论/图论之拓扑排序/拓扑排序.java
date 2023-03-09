40fa083b20f730c4ccd006751bcd19a2afcf8ad8
tomoyoooo <411167237@qq.com>
2023-03-09 17:38:30 +0800

    2023_3_9_changeDir

--- 图论/图论之拓扑排序/拓扑排序.java
+++ 图论/图论之拓扑排序/拓扑排序.java
@@ -41,3 +41,64 @@ class Solution {
         return res;
     }
 }
+
+
+//Leetcode 2192.有向无环图中一个节点的所有祖先
+//参考：https://leetcode.cn/problems/all-ancestors-of-a-node-in-a-directed-acyclic-graph/solution/java-tuo-bu-pai-xu-jian-dan-si-lu-by-rel-95yw/
+class Solution {
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        //构建两个线性表，一个作为返回值，另一个使用`TreeSet`，可以在去重的同时进行元素的排序
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Set<Integer>> demo = new ArrayList<>();
+
+        //构建零阶矩阵(邻接表会更好)，以及入度数组
+        int[] system = new int[n];
+        int[][] grid = new int[n][n];
+
+        //遍历`edges`，分别构建图中的边的关系以及各个节点的入度
+        for (int[] edge : edges) {
+            system[edge[1]]++;
+            grid[edge[0]][edge[1]] = 1;
+        }
+
+        //将图中入度为0的节点入队列，同时“填充好”我们最初构建的两个线性表
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (system[i] == 0) {
+                queue.offer(i);
+                system[i]--;
+            }
+            ans.add(new ArrayList<>());
+            demo.add(new TreeSet<>());
+        }
+
+        //分别对整个入度数组进行遍历，若入度为0，则加入队列，同时将该父节点以及该父节点的所有父节点加入线性表中
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            while (size-- > 0) {
+                int idx = queue.poll();
+                for (int i = 0; i < n; i++) {
+                    if (grid[idx][i] == 1) {
+                        system[i]--;
+                        demo.get(i).add(idx);
+                        demo.get(i).addAll(demo.get(idx));
+                    }
+                    if (system[i] == 0) {
+                        queue.offer(i);
+                        system[i]--;
+                    }
+                }
+            }
+        }
+
+        //将最终答案汇整到返回值`ans`中
+        for (int i = 0; i < n; i++) {
+            ans.get(i).addAll(demo.get(i));
+        }
+
+        return ans;
+    }
+}
+
